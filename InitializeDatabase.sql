d7709033965990e176db0da3a246f8d87ee066b6
Kane Chong <khchong@ucsd.edu>
2015-02-08 01:24:02 -0800

    Decided on using mongo..no more changing mind

--- InitializeDatabase.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Users (
-	id			SERIAL PRIMARY KEY,
-    first_name	varchar(255),
-    last_name	varchar(255),
-    username	varchar(255),
-    password	varchar(255),
-    email		varchar(255) 
-);
