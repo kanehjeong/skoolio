e7c6d8f43bb747ee146f009939ab8c5d86cc0e5e
Kane Chong <khchong@ucsd.edu>
2015-02-09 01:36:15 -0800

    Implemented Login System and Account Creation

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var User = require('../models/user');
+var mongoose = require('mongoose');
+
+var app = express();
 var router = express.Router();
+
+// get current db connection
+var db = mongoose.connection;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /* GET Pages */
 router.get('/', function(req, res, next) {
 	res.render('index');
 });
 
-router.get('/communities', function(req, res, next) {
-	res.render('communities');
+router.get('/account_created', function(req, res, next) {
+	res.render('account_created');
 });
 
 router.get('/curr_projects', function(req, res, next) {
@@ -18,5 +28,39 @@ router.get('/settings', function(req, res, next) {
 	res.render('settings');
 });
 
+/* POST */
+router.post('/', function(req, res, next) {
+
+    // attempt to authenticate user
+    User.getAuthenticated(req.body.email, req.body.password, function(err, user, reason) {
+        if (err) throw err;
+
+        // login was successful if we have a user
+        if (user) {
+            // handle login success
+            console.log('login success');
+            res.redirect('/homepage');
+            return;
+        }
+
+        // otherwise we can determine why we failed
+        var reasons = User.failedLogin;
+        switch (reason) {
+            case reasons.NOT_FOUND:
+            case reasons.PASSWORD_INCORRECT:
+                // note: these cases are usually treated the same - don't tell
+                // the user *why* the login failed, only that it did
+                res.send('Incorrect Login Information.');
+                break;
+            case reasons.MAX_ATTEMPTS:
+                // send email or otherwise notify user that account is
+                // temporarily locked
+                res.send('Exceeded Login Attempts. Account Locked for 5 Minutes.');
+                break;
+        }
+
+    });
+
+});
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var User = require('../models/user');
+var mongoose = require('mongoose');
+
+var app = express();
 var router = express.Router();
+
+// get current db connection
+var db = mongoose.connection;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /* GET */
 router.get('/signup', function(req, res, next) {
 	res.render('signup');
+});
+
+/* POST */
+router.post('/signup', function(req, res, next) {
+	console.log("Starting POST req and adding: " + req.body.fname);
+
+	var newUser = new User(req.body);
+
+	newUser.save(function(err) {
+		if(err) {
+			return res.send(err);
+		}
+		else {
+			res.redirect('/account_created')
+		}
+	});
 });
 
 module.exports = router;
