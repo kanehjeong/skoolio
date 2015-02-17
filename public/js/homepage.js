7e99044e4767f847a495a3aa978860f9a1752b50
Elizabeth Maraan <emaraan@gmail.com>
2015-02-17 01:49:23 -0800

    Able to add roles dynamically

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,27 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+/*$(document).ready(function() {
+})*/
+
+$('#add-role-form').click(function(e) {
+	//e.preventDefault();
+
+	var e1 = document.createElement("input");
+	e1.type = "text";
+	e1.className = "form-control";
+	e1.name = "roles";
+
+	var cont = document.getElementById("role-form");
+	cont.appendChild(e1);
+	cont.appendChild(document.createElement('br'));
+
+});
+
 function htmlbodyHeightUpdate(){
-		var height3 = $( window ).height()
-		var height1 = $('.nav').height()+50
-		height2 = $('.main').height()
+		var height3 = $( window ).height();
+		var height1 = $('.nav').height()+50;
+		var height2 = $('.main').height();
 		if(height2 > height3){
 			$('html').height(Math.max(height1,height3,height2)+10);
 			$('body').height(Math.max(height1,height3,height2)+10);
@@ -12,17 +32,18 @@ function htmlbodyHeightUpdate(){
 			$('body').height(Math.max(height1,height3,height2));
 		}
 		
-	}
-	$(document).ready(function () {
+}
+
+$(document).ready(function () {
+	htmlbodyHeightUpdate()
+	$( window ).resize(function() {
 		htmlbodyHeightUpdate()
-		$( window ).resize(function() {
-			htmlbodyHeightUpdate()
-		});
-		$( window ).scroll(function() {
-			height2 = $('.main').height()
-  			htmlbodyHeightUpdate()
-		});
 	});
+	$( window ).scroll(function() {
+		var height2 = $('.main').height()
+			htmlbodyHeightUpdate()
+	});
+});
 
 /*$(function() {
 	$("#nav-logout").on("click",function(e) {
