d5a0445eb1e0428d0194984d46c55e00b62fdc5c
Elizabeth Maraan <emaraan@gmail.com>
2015-02-17 15:44:46 -0800

    Added Description with show more/less text

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,9 +1,3 @@
-'use strict';
-
-// Call this function when the page loads (the "ready" event)
-/*$(document).ready(function() {
-})*/
-
 $('#add-role-form').click(function(e) {
 	//e.preventDefault();
 
@@ -35,15 +29,50 @@ function htmlbodyHeightUpdate(){
 }
 
 $(document).ready(function () {
-	htmlbodyHeightUpdate()
+	htmlbodyHeightUpdate();
 	$( window ).resize(function() {
-		htmlbodyHeightUpdate()
+		htmlbodyHeightUpdate();
 	});
 	$( window ).scroll(function() {
-		var height2 = $('.main').height()
-			htmlbodyHeightUpdate()
+		var height2 = $('.main').height();
+			htmlbodyHeightUpdate();
 	});
+
+	var showChar = 100;
+    var ellipsestext = "...";
+    var moretext = "more";
+    var lesstext = "less";
+    $('.more').each(function() {
+        var content = $(this).html().trim();
+ 
+ 		content = content.substr(28);       
+
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ ' </span><span class="morecontent"><span>' + h + '</span>  <a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
 });
+
 
 /*$(function() {
 	$("#nav-logout").on("click",function(e) {
