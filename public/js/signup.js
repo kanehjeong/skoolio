402009dfef1b175fee8964b32c779c52e113281b
Kane Chong <khchong@ucsd.edu>
2015-02-19 14:58:48 -0800

    Added signup js

---
+++ public/js/signup.js
@@ -0,0 +1,10 @@
+function validateForm() { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    //var email = $("input[name=email]").val();
+    //console.log(email);
+    //if(!re.test(email)){
+    	//alert("Email address is not valid");
+    	//return false;
+    //}
+} 
