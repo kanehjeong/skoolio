432530c8b3ca84c39f101959a25c6f956dedf51a
Kane Chong <khchong@ucsd.edu>
2015-02-25 03:38:33 -0800

    Added email syntax validation

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,13 +1,13 @@
-function validateForm() { 
+$('#btn-signup').click(function(e) {
+    var email = $('#signup-email').val();
     var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
-    //var email = $("input[name=email]").val();
-    //console.log(email);
-    //if(!re.test(email)){
-    	//alert("Email address is not valid");
-    	//return false;
-    //}
-} 
+
+    if(!re.test(email)) {
+        $('#incorrect-email').html("Not a valid email");
+        return false;
+    }
+
+});
 
 $(document).ready(function() {
     $(document).mousemove(function(event) {
@@ -18,6 +18,5 @@ $(document).ready(function() {
             	"background-position": parseInt(event.pageX / 8) + "px " + parseInt(event.pageY / 12) + "px, " + parseInt(event.pageX / 15) + "px " + parseInt(event.pageY / 15) + "px, " + parseInt(event.pageX / 30) + "px " + parseInt(event.pageY / 30) + "px"
             }
         })
-    })
-})
-
+    });
+});
