df934edb049c8a2983168e07e78a008a3ee7b3d5
Kane Chong <khchong@ucsd.edu>
2015-02-25 19:35:53 -0800

    Enable notifications functionality

--- models/notification.js
+++ models/notification.js
@@ -7,9 +7,18 @@ var NotificationSchema = new Schema({
 	toID: { type: String },
 	from: { type: String },
     to: { type: String },
+    createdAt: { type: Date },
     project: { type: String },
     roles: { type: [String] },
     message: { type: String }
+});
+
+// When creating a new project, get current date
+NotificationSchema.pre('save', function(next){
+	var now = new Date();
+
+	this.createdAt = now;
+	next();
 });
 
 module.exports = mongoose.model('Notification', NotificationSchema);
--- routes/notifications.js
+++ routes/notifications.js
@@ -2,13 +2,53 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var session = require('client-sessions');
 var User = require('../models/user');
+var Notification = require('../models/notification');
 
 var app = express();
 var router = express.Router();
 
 /* GET */
 router.get('/notifications', function(req, res, next) {
-	res.render('notifications');
+	// Check if session exists
+	if (req.session && req.session.user) {
+	    
+	    // lookup the user in the DB by pulling their email from the session
+	    User.findOne({ email: req.session.user.email }, function (err, user) {
+
+	    	if(err) {
+	    		res.send(err);
+	    		return;
+	    	}
+
+			// if the user isn't found in the DB, reset the session info and
+	        // redirect the user to the login page
+			if (!user) {
+
+	        	req.session.reset();
+	        	res.redirect('/');
+	    	} else {
+	        
+	        	// expose the user to the template
+	        	res.locals.user = user;
+
+	        	Notification.find({ 'toID': req.session.user._id }).sort({'createdAt' : 'desc'}).exec(function(err, notifications) {
+					
+					if(err) {
+						res.send(err);
+						return;
+					}
+
+					var nMap = {};
+					nMap["notifications"] = notifications;
+
+					res.render('notifications', nMap);
+				});
+			}	
+	    });
+		
+	} else {
+	    res.redirect('/');
+	}
 });
 
 module.exports = router;
